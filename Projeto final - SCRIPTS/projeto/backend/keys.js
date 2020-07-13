9c9327284108011e9ebe8fa2d5f8a9bcb2175a65
Rafael <rafaelmarquesclps9@gmail.com>
2020-07-13 16:56:49 -0300

    Projeto concluído

---
+++ Projeto final - SCRIPTS/projeto/backend/keys.js
@@ -0,0 +1,4 @@
+//é preciso definir o <usuario>, <senha> e <BD>. O BD deverá ser bdatv
+module.exports = {
+    mongoUrl: "mongodb+srv://RafaelMarques:rafaelsjcclps1@cluster0-tktrn.mongodb.net/<projetobd>?retryWrites=true&w=majority"
+}
